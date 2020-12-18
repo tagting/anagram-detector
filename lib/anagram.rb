9399ea628dcf4b80a5610825c7282e3f5a3aa6be
ting <ttzhenng@gmail.com>
2020-12-18 07:55:23 -0500

    commit tree ebd54888753a05559da1bcf9327cad509ae80eb1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word #take a word on initialization
+
+    def instance_match #respond to a match method
+        matches = word.scan#take in word
+    end
+
+end #end of Anagram class
+
+#pseduo code
+#take in word
+#are indexes the same
+#go over each letter do the letters
+#do the words match?
+#add to array
